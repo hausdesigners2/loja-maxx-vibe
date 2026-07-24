21f406dccb16936ec0df5f88b37cfa5bca5ce43c
[dyad] <hausdesigners2@gmail.com>
2026-07-24 10:12:05 -0300

    [dyad] Criando uma Splash Screen moderna e responsiva com ícone 3D em SVG e barra de progresso fluida - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { CartProvider } from "@/contexts/CartContext";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { SplashScreen } from "@/components/SplashScreen";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import CategoryPage from "./pages/CategoryPage";
@@ -31,6 +32,7 @@ const App = () => (
     <TooltipProvider>
       <AuthProvider>
         <CartProvider>
+          <SplashScreen />
           <Toaster />
           <Sonner theme="dark" position="top-center" />
           <BrowserRouter>
